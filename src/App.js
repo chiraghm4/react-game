f656ac793dbae9ed25ab06721dd8e24b6dc92387
chiraghm4 <chiragmaski4@gmail.com>
2024-02-11 23:02:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const newSet = new Set([]);
+  const [newArr, setNewArr] = useState([])
+  const allIcons = ["FaAtom", "FaAvianex", "FaArchway", "FaAngry", "FaBong", "FaCandyCane"]
+
+  useEffect(() => {
+    function mainFunc() {
+      while (true) {
+        newSet.add(0)
+        if(newSet.size === 10) 
+          break
+      }
+    }
+    mainFunc()
+    setNewArr(Array.from(newSet))
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: "100vh", backgroundColor: "black"}}>
+      <div className="flex-container">
+        <div>{newArr[0]}</div>
+        <div>1</div>
+        <div>1</div>
+      </div>
+      <div className="flex-container">
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+      </div>
+      <div className="flex-container row">
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+      </div>
     </div>
   );
 }
